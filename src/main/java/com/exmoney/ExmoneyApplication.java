c7d65813e3c7f0340520a23e0103a616e6b813aa
cuonvc <cuong.fithau@gmail.com>
2024-08-25 15:00:21 +0700

    init commit

---
+++ src/main/java/com/exmoney/ExmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.exmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exmoney/ExmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
