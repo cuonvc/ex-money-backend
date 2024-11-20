58c76c89771ba399b03858ff1779bb2bc82a9d65
cuonvc <cuong.fithau@gmail.com>
2024-11-20 22:14:24 +0700

    clean

--- src/main/java/com/exmoney/payload/response/user/UserResponse.java
+++ src/main/java/com/exmoney/payload/response/user/UserResponse.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class UserResponse {
-    private String id;
+    private Long id;
     private String name;
     private String email;;
     private String avatarUrl;
