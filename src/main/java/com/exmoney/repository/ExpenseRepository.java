7c11fd5cde1ab7a887c12cc5884001d6092feff8
Cuong Nguyen <cuong.fithau@gmail.com>
2024-12-11 00:16:01 +0700

    fix/get expense list by user

--- src/main/java/com/exmoney/repository/ExpenseRepository.java
+++ src/main/java/com/exmoney/repository/ExpenseRepository.java
@@ -23,7 +23,7 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
             "   , u1.name AS " + PROP_CREATED_BY + ", e.updatedAt AS " + PROP_UPDATED_AT + ", u2.name AS " + PROP_UPDATED_BY + ") " +
             "FROM Expense e " +
             "   INNER JOIN Wallet w ON e.walletId = w.id " +
-            "   INNER JOIN UserWallet uw ON uw.walletId = :walletId AND uw.userId = :currentUserId AND uw.status = 'ACTIVE' " +
+            "   LEFT JOIN UserWallet uw ON uw.walletId = :walletId AND uw.userId = :currentUserId AND uw.status = 'ACTIVE' " +
             "   INNER JOIN User u1 ON u1.id = e.userId " +
             "   LEFT JOIN User u2 ON u2.id = e.updatedBy " +
             "   INNER JOIN ExpenseCategory c ON c.id = e.categoryId " +
